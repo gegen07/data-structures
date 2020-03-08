1741bb84c9556a767241963edb7e4fb5f5f2f49f
Germano Barcelos <gegenbarcelos@gmail.com>
2020-03-07 23:40:40 -0300

    chore: implemented insert and traversing functions

--- C/bs-tree/lib/bs-tree.h
+++ C/bs-tree/lib/bs-tree.h
@@ -2,17 +2,18 @@
 #define __BS_TREE_H__
 
 #include "data.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct *node_t Node;
+typedef struct node_t *Node;
 struct node_t {
-  Data *data;
-  struct node *left;
-  struct node *right;
-}
+  Data data;
+  struct node_t *left;
+  struct node_t *right;
+};
 
 typedef struct {
   Node head;
-  int level;
 } bs_tree_t;
 
 typedef enum {pre_order, post_order, in_order} Order_print_t;
@@ -20,7 +21,7 @@ typedef enum {pre_order, post_order, in_order} Order_print_t;
 int node_init(Node *node, Data *data);
 
 int bst_init(bs_tree_t *tree);
-int bst_insert(bs_tree_t *tree, Node *node, int (*compare_node)(void*, void*));
+int bst_insert(Node *node, Node *node_insert, int (*compare_data)(void*, void*));
 int bst_print(bs_tree_t *tree, Order_print_t order_print);
 
 #endif
